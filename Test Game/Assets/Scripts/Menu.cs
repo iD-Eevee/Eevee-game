f3b7c2026d97bfd1b122330a4f531cd8a56f7561
Eevee <57205370+iD-Eevee@users.noreply.github.com>
2021-12-10 12:43:14 -0500

    Code for the new Menu
    Testing how to add a menu feature.

---
+++ Test Game/Assets/Scripts/Menu.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("This is the code for the menu");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
